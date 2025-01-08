31966e03041944d7a203d4f692bc2c764f1c077b
Carolina Moura <carolinamouravalle@yahoo.com>
2025-01-08 15:30:21 -0600

    Update send_image.py

--- send_image.py
+++ send_image.py
@@ -3,15 +3,19 @@ import time
 from PIL import Image
 
 BAUDRATE = 250000
-ARDUINO_PORT = '/dev/cu.usbmodem21301'
+ARDUINO_PORT = '/dev/cu.usbmodem1301'
+SAVE_PATH = "kris_2x3"
+IMAGE_PATH = "images/hambre.jpeg"
+WIDTH = 160
+HEIGHT = 240  
 
-arduino = serial.Serial(port=ARDUINO_PORT, baudrate=BAUDRATE, timeout=1)
-time.sleep(2)  # Allow time for Arduino to reset
+# arduino = serial.Serial(port=ARDUINO_PORT, baudrate=BAUDRATE, timeout=1)
+# time.sleep(2)  # Allow time for Arduino to reset
 
 
 # Load and Convert Image to RGB565
-def convert_image_to_rgb565(image_path):
-    img = Image.open(image_path).resize((10, 10), Image.Resampling.BILINEAR)  # Resize to match your display
+def convert_image_to_rgb565(image_path, width, height):
+    img = Image.open(image_path).resize((width, height), Image.Resampling.BILINEAR)  # Resize to match your display
     img = img.convert("RGB")  # Ensure RGB mode (discard alpha channel if any)
     rgb565_data = []
 
@@ -28,20 +32,22 @@ def convert_image_to_rgb565(image_path):
 
 
 # Send Image Data Over Serial
-def send_image_data(image_path):
-    rgb565_data = convert_image_to_rgb565(image_path)
+def send_image_data(image_path, save_path, width=WIDTH, height=HEIGHT):
+    rgb565_data = convert_image_to_rgb565(image_path, width, height)
     byte_array = bytearray()
+    byte_array.extend([width&0xFF, height&0xFF])
     for color in rgb565_data:
         high_byte = (color >> 8) & 0xFF
         low_byte = color & 0xFF
-        high_byte = low_byte = 0
         byte_array.extend([high_byte, low_byte])  # Add both bytes to the array
 
-    arduino.write(byte_array)  # Send all at once
-    with open("output_image", "wb") as f:
+    # arduino.write(byte_array)
+    # for b in byte_array:
+    #     arduino.write(b)
+    with open(save_path, "wb") as f:
         f.write(byte_array)
     print("Image data sent.")
 
 # Example Usage
-send_image_data("images/checkboard.png")
-arduino.close()
+send_image_data(IMAGE_PATH, SAVE_PATH)
+# arduino.close()
