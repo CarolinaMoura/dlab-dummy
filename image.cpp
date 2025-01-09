d45957e43ba9d618017d5411dd362ed103ca1774
su-elena <esu2021@gmail.com>
2025-01-09 12:23:29 -0600

    Image class additional functionality

--- image.cpp
+++ image.cpp
@@ -1,22 +1,63 @@
-
 #include "image.h"
+#include <Arduino.h>
+#include <SD.h>
+#include "Adafruit_GFX.h"
+#include <MCUFRIEND_kbv.h>
 
-Image::Image( const char* filename , const int image_id_num ) : 
+MCUFRIEND_kbv tft;
+
+Image::Image( const char* filename , const char* image_file , const int image_id_num ) : 
     row_index { 0 } ,
     image_id { image_id_num }
 
 {
     image_file = filename ;  
-    image_text = "" ; // until we decide what to do 
+    File file = SD.open(image_file , FILE_READ);
+
+    if (! file) {
+        Serial.println("Failed to open text file.");
+        return;
+    }
+
+    image_text = "" ;
+    size_t text_size = file.size() ; 
+
+    if ( text_size > 0 ) {
+        image_text = ( char* ) malloc( text_size + 1 ) ;
+
+        if ( !image_text ) {
+            Serial.println("Memory allocation failed.") ;
+            file.close() ;
+            return ;
+        }
+
+        file.readBytes(image_text, text_size) ;
+        image_text[ text_size ] = '\0' ;
+    }
+
+    file.close() ;
 }
 
-void Image::display( ) 
-{
+void Image::display() 
+{   
+    File image_file = SD.open(image_file) ;
     return ;
 }
 
 
 void Image::render_text() 
 {
+    if (! image_text ) {
+        Serial.println("No text available.") ;
+        return ;
+    }
+
+    tft.fillRect(0, TFT_HEIGHT , TFT_WIDTH , TFT_WIDTH - TFT_HEIGHT , TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextSize(TEXT_SIZE); 
+    tft.setCursor(0, TFT_HEIGHT);
+
+    const char* text = image_text;
+    
     return ;
 }
--- image.h
+++ image.h
@@ -4,20 +4,21 @@
 #define TFT_WIDTH   480
 #define TFT_HEIGHT  320
 #define IMG_DIMENSIONS 320 
+#define TEXT_SIZE 2
 
 #include <stdint.h> 
 
 class Image {
     public: 
         Image() = delete ; 
-        Image( const char* filename , const int image_id_num ) ;
+        Image( const char* filename , const char* image_file , const int image_id_num ) ;
 
         void display();
         void render_text(); 
     
     private: 
         const char* image_file; 
-        const char* image_text; 
+        char* image_text; 
         const int image_id;
         uint16_t row_buffer [ IMG_DIMENSIONS ] ;
         int row_index ; 
